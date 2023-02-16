7d4a733abe334fcd2a69893e08859bb78b4ab4f3
Rakshit <rakshitbang05@gmail.com>
2023-02-16 23:01:46 +0530

    Added Navbar

--- src/App.js
+++ src/App.js
@@ -1,28 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+import { Navbar } from "./components/Navbar";
 
 function App() {
   return (
-    <div>
-      <h1> Hello Rakshit </h1>
-    </div>
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
-  );
+    <>
+      <Navbar/>
+    </>
+   );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   
 }
 
 export default App;
+
+
+
