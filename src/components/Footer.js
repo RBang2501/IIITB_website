2372184d7bd5475b6b2fa95cb739fd7a4dab2854
Rahul Jain <rahulcom64@gmail.com>
2023-02-20 03:46:18 +0530

    Instagram Embeded

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -21,8 +21,8 @@ export const Footer = () => {
       className="text-center text-lg-start text-muted"
       style={{ background: "rgb(10, 25, 41)" }}
     >
-      <section class="d-flex justify-content-center justify-content-lg-between p-3 border-bottom">
-        <div class="me-5 d-none d-lg-block">
+      <section className="d-flex justify-content-center justify-content-lg-between p-3 border-bottom">
+        <div className="me-5 d-none d-lg-block">
           <span>Get connected with us on social networks:</span>
         </div>
 
--- src/components/SocialMediaEmbed.js
+++ src/components/SocialMediaEmbed.js
@@ -1,13 +1,23 @@
 import { InstagramEmbed, LinkedInEmbed, TwitterEmbed } from 'react-social-media-embed';
+import "../assets/styles/embed.css"
 
 export const SocialMediaEmbed = () => {
     return (
         <>
-            {/* <div style={{ display: 'flex', justifyContent: 'center' }}>
-                <InstagramEmbed captioned="false" url="https://www.instagram.com/p/CoZiWXuyYXR/?utm_source=ig_web_copy_link" width={328} />
-                <InstagramEmbed captioned="false" url="https://www.instagram.com/p/CoZuU79S0u5/?utm_source=ig_web_copy_link" width={328} />
+            <div className='d-flex'>
+                <div className='w-50'></div>
+                <div className='w-50'>
+                <div className='insta-container my-0 py-0' style={{ display: 'flex' }}>
+                    <InstagramEmbed className='instaEmbed' captioned="false" url="https://www.instagram.com/p/CoZiWXuyYXR/?utm_source=ig_web_copy_link" width={328} />
+                    <InstagramEmbed className='instaEmbed' captioned="false" url="https://www.instagram.com/p/CoZuU79S0u5/?utm_source=ig_web_copy_link" width={328} />
+                </div>
+                <div className='insta-container my-0 py-0' style={{ display: 'flex', transform:'translateY(-55px)'}}>
+                    <InstagramEmbed className='instaEmbed' captioned="false" url="https://www.instagram.com/p/CocPkCqvZhp/?utm_source=ig_web_copy_link" width={328} />
+                    <InstagramEmbed className='instaEmbed' captioned="false" url="https://www.instagram.com/p/CoZiAAcyudW/?utm_source=ig_web_copy_link" width={328} />
+                </div>
+                </div>
             </div>
-            
+
             <TwitterEmbed
                 url="https://twitter.com/IIITB_official/status/1626526926398685184?ref_src=twsrc%5Etfw"
                 width={325}
@@ -19,7 +29,7 @@ export const SocialMediaEmbed = () => {
                     height={863}
                     width={564}
                 />
-            </div> */}
+            </div>
         </>
     );
 }
