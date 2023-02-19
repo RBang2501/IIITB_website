7182c8594c4b9e844bbff02f67c3b45ce3e8e7cd
Rakshit <rakshitbang05@gmail.com>
2023-02-19 16:03:50 +0530

    Added Loader Animation

--- src/App.js
+++ src/App.js
@@ -1,48 +1,61 @@
-import './App.css';
+// import './App.css';
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap/dist/js/bootstrap.bundle'
+import '../src/assets/styles/loader.css'
+import college_logo from "./assets/college_logo.jpg";
 
 import { Navbar } from "./components/Navbar";
 import { Megamenu } from "./components/Megamenu";
 import { EventCarousel} from "./components/EventCarousel"
-import { Footer } from "./components/Footer";
+import { Footer } from "./components/Footer"
+import {useState, useEffect} from 'react'
 
-function App() {
+export const App = () => {
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+  }, []);
+
   return (
-    <>
-      <Navbar/>
-      {/* <Megamenu/> */}
-      {/* <EventCarousel/> */}
-      <Footer/>
-    </>
-   );
+    <div>
+      {loading ? (
+        <div>
+          <div>
+            <img
+              className="loader_logo"
+              src={college_logo}
+            />
+          </div>
+          
+          <div className="loader-container">
+            <div className="spinner"></div>
+          </div>
+        </div>
+      ) : (
+        <>
+          <Navbar />
+          <h1>Rakshit Bang</h1>
+        </>
+      )}
+    </div>
+  );
+};
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-}
-
-export default App;
+// function App() {
+//   return (
+//     <>
+//       <Navbar/>
+//       {/* <Megamenu/> */}
+//       {/* <EventCarousel/> */}
+//       <Footer/>
+//     </>
+//    );
 
 
 
